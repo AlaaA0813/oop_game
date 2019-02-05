e202c18e853eceab787b7b77d7f251acaf0ad3ab
Alaa Ayyash <aja5063@gmail.com>
2019-02-05 12:14:13 -0500

    creasted class Scene

--- ex45.py
+++ ex45.py
@@ -1,1 +1,14 @@
-# OOP Game
+# Cave Escape!
+
+    # Learn Python the Hard Way - Zed Shaw
+    # Exercise 43, pages 164 - 177
+    # Using Shaw's class skeleton and code to help build my game.
+
+from sys import exit # will close an infinity loop
+from textwrap import dedent # remove indentation from strings
+
+class Scene(object):
+    def enter(self):
+        print("This scene is not yet configured.")
+        print("Subclass it and implement enter()")
+        exit(1) # end loop
