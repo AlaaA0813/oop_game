8dd3a3ca3cd01c09422116435725a4da368d4940
Alaa Ayyash <aja5063@gmail.com>
2019-02-05 16:32:06 -0500

    added class Engine

--- ex45.py
+++ ex45.py
@@ -12,3 +12,15 @@ class Scene(object):
         print("This scene is not yet configured.")
         print("Subclass it and implement enter()")
         exit(1) # end loop
+
+class Engine(object):
+    def __init__(self, scene_map):
+        self.scene_map = scene_map
+
+    def play(self):
+        current_scene = self.scene_map.opening_scene() # current_scene is also the opening_scene
+        last_scene = self.scene_map.next_scene('finished') # when we call 'finished', the last_scene gets ranself.
+        while current_scene != last_Scene:
+            next_scene_name = current_scene.enter() # make the next_scene be the current_scene
+            current_scene = self.scene_map.next_scene(next_scene_name) # make current_scene the next_scene_name
+        current_scene.enter() # print out the map
