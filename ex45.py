83cc4f60911e752e1d6eb865c110084d8e96cc9e
Alaa Ayyash <aja5063@gmail.com>
2019-02-05 20:46:42 -0500

    fixed small grammar errors

--- ex45.py
+++ ex45.py
@@ -1,7 +1,7 @@
 # Cave Escape!
 
     # Learn Python the Hard Way - Zed Shaw
-    # Exercise 43, pages 164 - 177
+    # Exercise 43: Basic Object-Oriented Analysis and Design, pages 164 - 177
     # Using Shaw's class skeleton and code to help build my game.
 
 from sys import exit # will close an infinity loop
